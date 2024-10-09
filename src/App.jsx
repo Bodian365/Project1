614631ea645d74f01a675d3ba6a40fbff7b0b4f3
Bodian365 <ivanyna.bohdan@student.uzhnu.edu.ua>
2024-10-09 16:56:57 +0300

    creating a layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
+// import resizeHook from '@hooks/resize';
 import styles from './App.css';
+// import * as styles from './App.css';
 
 const App = () => {
-  return (
-    <div className="styles.main">My first application</div>
-  )
+    return (
+        <div className={styles.main} data-testid="app">
+            Projecct 1
+        </div>
+        
+    );
 };
 
 // export { App };
